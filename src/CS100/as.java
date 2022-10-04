a84fd261432778ed8184fbdf9e167341af43007a
Ahmed Abasiyanik <76453946+aabasiyanik@users.noreply.github.com>
2022-10-04 14:24:13 -0500

    change

---
+++ src/CS100/as.java
@@ -0,0 +1,7 @@
+package CS100;
+
+public class as {
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+    }
+}
--- src/CS201/sa.java
+++ src/CS201/sa.java
@@ -1,4 +1,7 @@
 package CS201;
 
 public class sa {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
